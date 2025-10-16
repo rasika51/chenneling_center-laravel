5cb6914eaa30cf1d9b9069afe25e620af2a22101
Rasika Wedaarachchi <wedaarachchirasika@gmail.com>
2025-10-16 23:27:49 +0530

    Update login blade file

--- resources/views/page/Login.blade.php
+++ resources/views/page/Login.blade.php
@@ -7,27 +7,37 @@
             <div class="row justify-content-center position-relative" style="margin-top: -200px; z-index: 1;">
                 <div class="col-lg-8">
                     <div class="bg-white boddyofconetend rounded p-5 m-5 mb-0">
+                        <!-- Error Messages -->
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul class="mb-0">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
                         <form method="POST" action="{{ url('logindata') }}">
                             @csrf
                             <div class="row g-3">
 
                                 <div class="col-12">
-                                    <select class="form-control bg-light border-0" style="height: 55px;" name='role'>
-                                        <option value="1" selected>Select Role</option>
-                                        <option value="1">Admin</option>
-                                        <option value="2">Patient</option>
-                                        <option value="3">Doctor</option>
+                                    <select class="form-control bg-light border-0" style="height: 55px;" name='role' required>
+                                        <option value="">Select Role</option>
+                                        <option value="1" {{ old('role') == '1' ? 'selected' : '' }}>Admin</option>
+                                        <option value="2" {{ old('role') == '2' ? 'selected' : '' }}>Patient</option>
+                                        <option value="3" {{ old('role') == '3' ? 'selected' : '' }}>Doctor</option>
                                     </select>
-
                                 </div>
 
                                 <div class="col-12">
                                     <input type="email" class="form-control bg-light border-0" placeholder="Email"
-                                        name="email" style="height: 55px;">
+                                        name="email" value="{{ old('email') }}" style="height: 55px;" required>
                                 </div>
                                 <div class="col-12">
                                     <input type="password" class="form-control bg-light border-0" placeholder="Password"
-                                        name="password" style="height: 55px;">
+                                        name="password" style="height: 55px;" required>
                                 </div>
                                 <div class="col-12">
                                     <button class="btn btn-primary w-100 py-3" type="submit">Login</button>
