c6976d46f66af05e665c012851b0c03bfefa7c14
Rasika Wedaarachchi <wedaarachchirasika@gmail.com>
2025-10-16 23:27:28 +0530

    Update patientModel.php

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -3,23 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Patient extends Model
+class Patient extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+    
     protected $table = 'patients';
+    
     protected $fillable = [
         "full_name",
-    "email",
-    "gender",
-    "dob",
-    "contact_no",
-    "address",
-    "marital_status",
-    "allergic_medicine",
-    "password"
+        "email",
+        "gender",
+        "dob",
+        "contact_no",
+        "address",
+        "marital_status",
+        "allergic_medicine",
+        "password"
+    ];
 
-    ] ;
-    public $timestamps = false; // This line will disable the automatic timestamp columns
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
 }
